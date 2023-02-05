fc60bcb63080334ebdbb363e1894adb8a4c9e2c4
IsuruAhinsa <isurujayawickramat@gmail.com>
2023-02-05 16:15:58 +0530

    Server.updated

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ app.use(cors());
 app.use(express.json());
 const path = require("path");
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, 'build')));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, 'build')));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//   });
+// }
 
 app.get("/", (req, res) => {
   res.send("Welcome to ecommerce platform");
